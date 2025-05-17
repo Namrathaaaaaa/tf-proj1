092698f92dea3aa1a88efa40d55c3809d44628b8
Namratha <namratha343@gmail.com>
2025-05-17 19:31:28 +0530

    index,error n main

--- main.tf
+++ main.tf
@@ -24,3 +24,43 @@ resource "aws_s3_bucket_public_access_block" "example" {
 }
 
 
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example
+  ]
+
+  bucket = aws_s3_bucket.my-bucket.id
+  acl    = "public-read"
+
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.my-bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.my-bucket.id
+  key          = "error.html"
+  source       = "error.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.my-bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+  depends_on = [aws_s3_bucket_acl.example]
+
+}
+
+

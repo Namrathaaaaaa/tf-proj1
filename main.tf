abdb367be90704bbab8b3aa1d414bbf2aa22caab
Namratha <namratha343@gmail.com>
2025-05-17 18:36:35 +0530

    main

--- main.tf
+++ main.tf
@@ -5,10 +5,22 @@ resource "aws_s3_bucket" "my-bucket" {
 }
 
 
-resource "aws_s3_bucket_ownership_controls" "s3-public" {
-  bucket = aws_s3_bucket.my-bucket
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.my-bucket.id
   rule {
-    object_ownership = ""
+    object_ownership = "BucketOwnerPreferred"
   }
 
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.my-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+

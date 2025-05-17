eea4a79d998ba8c6e16e975f0e6f7aaf9361faf8
Namratha <namratha343@gmail.com>
2025-05-17 18:32:22 +0530

    bucket ownership

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@
 resource "aws_s3_bucket" "my-bucket" {
   bucket = var.bucketname
 }
+
+
+resource "aws_s3_bucket_ownership_controls" "s3-public" {
+  bucket = aws_s3_bucket.my-bucket
+  rule {
+    object_ownership = ""
+  }
+
+}
